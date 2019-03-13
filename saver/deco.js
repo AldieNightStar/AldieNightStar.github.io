54459d6e17a25dc11790f7ae39f859ea53886ad2
Ihor Melnyk <imelnyk@corevalue.net>
2019-03-13 17:59:14 +0200

    1

--- saver/deco.js
+++ saver/deco.js
@@ -42,3 +42,13 @@ function makeDataUrl(data) {
 function editDataUrl(data) {
 	return baseSite_edit + specialPrefix + toBase(data);
 }
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
